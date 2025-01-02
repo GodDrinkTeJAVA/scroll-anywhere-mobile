6a64282d3962a9f622523598a07fccb0bd715345
이준희 <jun-hee.lee@navercorp.com>
2025-01-02 09:08:43 +0900

    feat: Add scroll event

--- src/scroll/scrollbar-area.ts
+++ src/scroll/scrollbar-area.ts
@@ -2,15 +2,32 @@ import ScrollbarTouch from "./scrollbar-touch.ts";
 
 export default class ScrollbarArea {
   private scrollbarAreaElement: HTMLDivElement;
-  private scrollbarTouch: ScrollbarTouch;
+  private scrollbarTouch: ScrollbarTouch | undefined;
   constructor() {
-    this.scrollbarTouch = new ScrollbarTouch();
     this.scrollbarAreaElement = document.createElement("div");
     this.scrollbarAreaElement.className = "scrollbar-area";
-    this.scrollbarAreaElement.appendChild(this.scrollbarTouch.element);
+    const boundHandleScroll = this.handleScroll.bind(this);
+    const boundHandleScrollEnd = this.handleScrollEnd.bind(this);
+    document.addEventListener("scroll", boundHandleScroll);
+    // document.addEventListener('scrollend', boundHandleScrollEnd);
   }
 
   get element() {
     return this.scrollbarAreaElement;
+  }
+
+  private handleScroll() {
+    if (this.scrollbarTouch) {
+        const newHeight = window.innerHeight * (window.scrollY / document.body.scrollHeight);
+        this.scrollbarTouch.move(newHeight)
+        return;
+    }
+    this.scrollbarTouch = new ScrollbarTouch();
+    this.scrollbarAreaElement.appendChild(this.scrollbarTouch.element);
+  }
+
+  private handleScrollEnd() {
+    this.scrollbarTouch?.destroy();
+    this.scrollbarTouch = undefined;
   }
 }
--- src/scroll/scrollbar-touch.ts
+++ src/scroll/scrollbar-touch.ts
@@ -6,7 +6,12 @@ export default class ScrollbarTouch {
   }
 
   public move(newHeight: number) {
+    console.log(newHeight)
     this.moveByPixel(newHeight);
+  }
+
+  public destroy() {
+    this.scrollbarTouchElement.remove();
   }
 
   get element() {
@@ -16,7 +21,7 @@ export default class ScrollbarTouch {
   private createDOMElement() {
     this.scrollbarTouchElement = document.createElement("button");
     const windowHeight = document.body.scrollHeight;
-    const heightRatio = document.body.scrollTop / window.innerHeight;
+    const heightRatio = window.scrollY / window.innerHeight;
     this.scrollbarTouchElement.style.transform = `translateY(${windowHeight * heightRatio})`;
     const scrollbarHeight = Math.pow(window.screen.height, 2) / document.body.scrollHeight;
     this.scrollbarTouchElement.style.height = `${scrollbarHeight}px`;
@@ -41,13 +46,13 @@ export default class ScrollbarTouch {
 
   private moveByPixel(newHeight: number) {
     this.scrollbarTouchElement.style.transform = `translateY(${newHeight}px)`;
-    const windowHeight = document.body.scrollHeight;
-    const heightRatio = newHeight / window.innerHeight;
-    window.scroll({top: windowHeight * heightRatio});
   }
 
   private touchMoveHandler(event: TouchEvent) {
     const newHeight = event.touches[0].clientY;
     this.moveByPixel(newHeight);
+    const windowHeight = document.body.scrollHeight;
+    const heightRatio = newHeight / window.innerHeight;
+    window.scroll({top: windowHeight * heightRatio});
   }
 }
